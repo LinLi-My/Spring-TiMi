79dbd9e555780e2eeabe9055e1a8f3ad20745656
Lin <471429994@qq.com>
2021-06-11 10:23:47 +0800

    项目基本架构搭建

---
+++ src/test/java/com/ml/timi/springtimi/SpringTiMiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.timi.springtimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTiMiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
